7916d3e9402c6c65cced0e5c83e70f9cda087f23
Daniel Gustavo <dg520589@gmail.com>
2024-05-22 18:00:44 -0300

    wip: setup project

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Inter } from 'next/font/google';
+
+import type { Metadata } from 'next';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'High',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
