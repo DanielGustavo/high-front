f2963ebf322eaec19ddec606e77914e0aa069d10
Daniel Gustavo <dg520589@gmail.com>
2024-05-25 11:11:20 -0300

    fix: styles were taking a time to fully load

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -6,15 +6,19 @@ import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from '@/styles/global';
 import { theme } from '@/styles/theme';
 
+import StyledComponentsRegistry from './StyledComponentsRegistry';
+
 export default function Providers({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      {children}
-    </ThemeProvider>
+    <StyledComponentsRegistry>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        {children}
+      </ThemeProvider>
+    </StyledComponentsRegistry>
   );
 }
